fef9ec868f7506f4fd93fc87ba242be73dc11128
Micah Banschick <micha.ban@gmail.com>
2020-12-19 20:37:06 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,18 @@
+require 'pry'
+
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
     @product = Product.new
+    @cart = cart
   end
 
   def add
     # @product = Product.new
-    cart << params[:product][:name]
+    # binding.pry
+
+    cart << params[:product]
+    render :index
   end
 end
