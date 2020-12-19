3308cc4c0d1a11ae7fa0c21c44ed8ea2e8d132ed
Micah Banschick <micha.ban@gmail.com>
2020-12-19 19:19:43 +0000

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    @product = Product.new
+  end
+
+  def add
+    # @product = Product.new
+    cart << params[:product][:name]
+  end
+end
